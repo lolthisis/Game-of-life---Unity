2aeedc4421d053a79ec0473dc38fefb2046d5385
Puneet Rawat <puneetrawat9995@gmail.com>
2019-10-04 00:45:09 +0530

    Full unity code commit

---
+++ The Game Of Life/Assets/Scripts/cellManager.cs
@@ -0,0 +1,51 @@
+﻿/// <summary>
+/// Cell manager.
+/// Coded by Puneet Rawat
+/// </summary>
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cellManager : MonoBehaviour
+{
+    bool alive;
+
+    public List<cellManager> neighbors;
+    RawImage img;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Init
+        img = this.GetComponent<RawImage>();
+    }
+
+    public void willBeAlive(float value)
+    {
+        alive = Random.Range(0.0f, 1.0f) <= value ? true : false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alive)
+            img.color = Color.black;
+        else
+            img.color = Color.white;
+    }
+
+    public void tick()
+    {
+        int count = 0;
+        for(int i = 0; i < neighbors.Count; i++)
+        {
+            if(neighbors[i].alive)
+                count++;
+        }
+        if (count == 3)
+            alive = true;
+        else if (count < 2 || count > 3)
+            alive = false;
+    }
+}
---
+++ The Game Of Life/Assets/Scripts/gameManager.cs
@@ -0,0 +1,128 @@
+﻿/// <summary>
+/// Game manager.
+/// Spawns cell and mananges the tik time
+/// </summary>
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cellPrefab, panel;
+
+    [Header("Input no:")]
+    [SerializeField]
+    int row = 6;
+    [SerializeField]
+    int col=6;
+
+    [Header("Time between each tik:")]
+    [SerializeField]
+    float tikInterval=0.1f;
+
+    cellManager[][] cells;
+
+    GridLayoutGroup layout;
+
+    [Header("% chance for cell to start alive")]
+    [SerializeField]
+    [Range(0f,100f)]
+    float startingFrequency=50f;
+
+    bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Extensibility
+        layout = panel.GetComponent<GridLayoutGroup>();
+        layout.cellSize = Vector2.one * Screen.height / row;
+
+        if (row < col)
+        {
+            layout.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+            layout.constraintCount = row;
+        }else if (col < row)
+        {
+            layout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            layout.constraintCount = col;
+        }
+
+        //Init
+        cells = new cellManager[row][];
+
+        //Spawn cells
+        for(int i = 0; i < row; i++)
+        {
+            cells[i] = new cellManager[col];
+            for(int j = 0; j < col; j++)
+            {
+                GameObject temp = Instantiate(cellPrefab);
+                temp.transform.SetParent(panel.transform);
+                temp.transform.localScale = Vector3.one;
+                cells[i][j] = temp.GetComponent<cellManager>();
+                cells[i][j].willBeAlive(startingFrequency/100f);
+            }
+        }
+
+        //Assigning Neighbors
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i-1>=0&&j-1>=0)
+                    cells[i][j].neighbors.Add(cells[i - 1][j - 1]);
+                if (i-1>=0)
+                    cells[i][j].neighbors.Add(cells[i - 1][j]);
+                if (i+1<row)
+                    cells[i][j].neighbors.Add(cells[i + 1][j]);
+                if (i+1<row&&j-1>=0)
+                    cells[i][j].neighbors.Add(cells[i + 1][j - 1]);
+
+                if (i+1<row&&j+1<col)
+                    cells[i][j].neighbors.Add(cells[i + 1][j + 1]);
+                if (j-1>=0)
+                    cells[i][j].neighbors.Add(cells[i][j - 1]);
+                if (j+1<col)
+                    cells[i][j].neighbors.Add(cells[i][j + 1]);
+                if (i-1>=0&&j+1<col)
+                    cells[i][j].neighbors.Add(cells[i - 1][j + 1]);
+            }
+        }
+
+        //Start ticking
+        StartCoroutine(globalTick());
+    }
+
+    IEnumerator globalTick()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(tikInterval);
+            if (!isPaused)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        cells[i][j].tick();
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Pause/Play
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPaused = !isPaused;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        StopAllCoroutines();
+    }
+}
